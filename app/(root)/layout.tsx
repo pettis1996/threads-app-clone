747769e2b11312b450bdd31b5590ea486aef5fb9
Paris Pettis <pettis_paris@hotmail.com>
2023-08-28 23:10:05 +0300

    update
    update on app's structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,11 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+import RightSidebar from '@/components/shared/RightSidebar'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +24,21 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className="{`$inter.className bg-dark-1`}">
-          {children}
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className= "main-container">
+              <div className="w-full max-w-4x1">
+                {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
